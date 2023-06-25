30500ebc1b00ebd6037dd4634200e8489fbd7adb
Alexey Pogodin <alex-alexej@bk.ru>
2023-06-26 00:43:05 +0300

    Сделал

--- test.py
+++ test.py
@@ -1,18 +1,11 @@
 from selene import browser, have, be
 import pytest
-def test_complete_todo():
-    browser.open('https://todomvc.com/examples/emberjs/')
-    browser.element('[id="new-todo"]').should(be.blank)
-
-    browser.element('[id="new-todo"]').type('a').press_enter()
-    browser.element('[id="new-todo"]').type('b').press_enter()
-    browser.element('[id="new-todo"]').type('c').press_enter()
-    browser.all('#todo-list>li').should(have.size(3))
+import os
 
 @pytest.fixture(scope='session', autouse=True)
 def window_size():
-    browser.config.window_width = 1024
-    browser.config.window_height = 768
+    browser.config.window_width = 1920
+    browser.config.window_height = 1068
 
 def test_form_demoqa():
     browser.open('https://demoqa.com/automation-practice-form')
@@ -22,17 +15,33 @@ def test_form_demoqa():
     browser.element('[id="lastName"]').should(be.blank).type('Погодин')
     browser.element('[id="userEmail"]').should(be.blank).type('test@test.ru')
     browser.element('[for="gender-radio-1"]').click()
-    browser.element('[id="userNumber"]').should(be.blank).type('89999999999')
-    browser.element('[id="dateOfBirthInput"]').click()
-    browser.element('.react-datepicker__day--026').click()
+    browser.element('[id="userNumber"]').should(be.blank).type('89040527687')
+    browser.element('#dateOfBirthInput').click()
+    browser.element('.react-datepicker__year-select').click()
+    browser.element('[value="1989"]').click()
+    browser.element('.react-datepicker__month-select').click()
+    browser.element('[value="11"]').click()
+    browser.element('.react-datepicker__day--024').click()
     browser.element('[id="subjectsInput"]').should(be.blank).type('Test')
     browser.element('[for="hobbies-checkbox-1"]').click()
-    browser.element('[id="uploadPicture"]').send_keys(os.path.abspath('ios_large_1550063059_image.jpg'))
+    browser.element('#uploadPicture').send_keys(os.path.abspath('images/ios_large_1550063059_image.jpg'))
     browser.element('[id="currentAddress"]').should(be.blank).type('улица Нижегородская, 125')
     browser.element('[id="state"]').click()
-    browser.element('[id="state"]').set('NCR')
-    browser.element('[id="state"]').set('Delhi')
+    browser.all("#state div").element_by(have.exact_text("NCR")).click()
+    browser.element('#city').click()
+    browser.all("#city div").element_by(have.exact_text("Delhi")).click()
 
     browser.element('[id="submit"]').click()
 
 
+#проверяем форму
+    browser.element('.table-responsive').should(have.text('Алексей Погодин'))
+    browser.element('.table-responsive').should(have.text('test@test.ru'))
+    browser.element('.table-responsive').should(have.text('Male'))
+    browser.element('.table-responsive').should(have.text('24 December,1989'))
+    browser.element('.table-responsive').should(have.text('Sports'))
+    browser.element('.table-responsive').should(have.text('ios_large_1550063059_image.jpg'))
+    browser.element('.table-responsive').should(have.text('улица Нижегородская, 125'))
+    browser.element('.table-responsive').should(have.text('NCR Delhi'))
+
+    browser.element('[id="closeLargeModal"]').click()
